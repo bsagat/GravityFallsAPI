3ffa5d61d21b25c72c7dcacc02ddff929385e71a
bsagat <sagatbekbolat854@gmail.com>
2025-06-12 12:27:30 +0500

    fix: exception error

--- main.py
+++ main.py
@@ -26,23 +26,21 @@ def get_characters_count():
 
 @app.get("/characters/{character_id}", response_model=Character)
 def get_character(character_id: int):
-    try:
-        count = repo.CharacterCount()
-        if character_id <= 0 or character_id > count:
-            raise HTTPException(status_code=404, detail=f"Character ID must be between 1 and {count}")
+
+    count = repo.CharacterCount()
+    if character_id <= 0 or character_id > count:
+        raise HTTPException(status_code=404, detail=f"Character ID must be between 1 and {count}")
         
-        character = repo.CharacterByID(character_id)
+    character = repo.CharacterByID(character_id)
 
-        if character is None:
-            raise HTTPException(status_code=404, detail="Character not found")
+    if character is None:
+        raise HTTPException(status_code=404, detail="Character not found")
 
-        return {
-            "Id": character[0],
-            "Name": character[1],
-            "Species": character[2],
-            "Likes": character[3],
-            "Quote": character[4],
-            "Image": character[5]
-        }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return {
+        "Id": character[0],
+        "Name": character[1],
+        "Species": character[2],
+        "Likes": character[3],
+        "Quote": character[4],
+        "Image": character[5]
+    }
